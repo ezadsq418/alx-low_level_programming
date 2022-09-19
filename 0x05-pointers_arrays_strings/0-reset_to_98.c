6ec0254530702315e848b80089e27183d23ee872
ezadsq418 <mouna22aloui@gmail.com>
2022-09-19 14:13:18 -0700

    TASK0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * reset_to_98 - takes a point of an int paramater and updates it to 98
+ *
+ * @n: int paramater
+ *
+ * Return:  Nothing
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
