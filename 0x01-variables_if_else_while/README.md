8a7b41adff9a82bbb0dffee36e722e5d43e99ea7
ezadsq418 <mouna22aloui@gmail.com>
2022-09-22 09:47:10 -0700

    Low-level

---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+tasks for if else while c language
---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - Sums two integers and
+ * return the result.
+ *
+ * @num1: integer one.
+ * @num2: integer two.
+ *
+ * Return: Sum of the two integers
+ */
+
+int add(int num1, int num2)
+{
+
+	return (num1 + num2);
+}
