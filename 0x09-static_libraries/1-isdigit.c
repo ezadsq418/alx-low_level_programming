93ba6fcc6cea2e440130dc0ba76842d0e4b47ef9
ezadsq418 <mouna22aloui@gmail.com>
2022-10-04 13:18:19 -0700

    static lib

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for digits
+ *
+ * @c: Number to be checked
+ *
+ * Return: 1 (Success) OR (Failure)
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+
+	return (0);
+}
